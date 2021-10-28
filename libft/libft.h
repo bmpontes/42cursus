103708c3f89894913b51f3018dc1097f18df74ef
bmpontes <86714209+bmpontes@users.noreply.github.com>
2021-10-28 15:13:32 +0100

    Add files via upload

---
+++ libft/libft.h
@@ -0,0 +1,45 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+/* 
+Part 1 
+*/
+
+int	ft_atoi(const char	*str);
+int	ft_isalnum(int	c);
+int	ft_isalpha(int	c);
+int	ft_isascii(int	c);
+int	ft_isdigit(int	c);
+int	ft_isprint(int	c);
+int	ft_toupper(int	c);
+int	ft_tolower(int	c);
+
+int	ft_strncmp(const char	*s1, const char	*s2, size_t	n);
+int	ft_memcmp(const void	*s1, const void	*s2, size_t	n);
+
+void	*ft_memchr(const void	*s, int	c, size_t	n);
+void	*ft_memcpy(void *dst, const void *src, size_t	n);
+void	*ft_memmove(void	*dst, const void	*src, size_t	len);
+void	*ft_memset(void	*b, int	c, size_t	len);
+void	*ft_calloc(size_t	nmemb, size_t	size);
+void	ft_bzero(void	*s, size_t	n);
+
+char	*ft_strchr(const char	*s, int	c);
+char	*ft_strdup(const char	*s);
+char	*ft_strrchr(const char	*s, int	c);
+char	*ft_strnstr(const char *str, const char *to_find, size_t size);
+
+size_t	ft_strlen(const char	*str);
+size_t	ft_strlcat(char	*dest, const char	*src, size_t	size);
+size_t	ft_strlcpy(char	*dest, const char	*src, size_t	size);
+
+/* 
+Part 2
+*/
+
+
+
+#endif
---
+++ libft/testes/main.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	if (ft_isdigit('2') == 1)
+		printf("É\n");
+	else
+		printf("Não É\n");
+	return (0);
+}

70520da4bf27e3909e4f2d8bea0fce1f0053e6dc
bmpontes <86714209+bmpontes@users.noreply.github.com>
2021-11-16 10:54:20 +0000

    Add files via upload

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,56 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: apuchill <apuchill@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/31 14:43:26 by apuchill          #+#    #+#             */
-/*   Updated: 2020/02/14 00:24:49 by apuchill         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
-#include <stdio.h>
-
-size_t  ft_strlen(const char    *s)
-{
-        size_t  c;
-
-        c = 0;
-        while (s[c] != 0)
-                c++;
-        return (c);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*new_str;
-	int		i;
-	int		j;
+	char	*s3;
+	int	i;
+	int	c;
 
-	if (!s1 || !s2 ||
-		!(new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+	if (!s1 || !s2 || 
+		!(s3 = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (0);
 	i = 0;
-	while (s1)
+	c = 0;
+	while (s1[i])
 	{
-		new_str[i] = s1[i];
+		s3[i] = s1[i];
 		i++;
 	}
-	j = 0;
-	while (s2)
+	while (s2[c])
 	{
-		new_str[i] = s2[j];
+		s3[i] = s2[c];
 		i++;
-		j++;
+		c++;
 	}
-	new_str[i] = '\0';
-	return (new_str);
+	s3[i] = '\0';
+	return (s3);
 }
 
-int     main(void)
-{
-        printf("%s\n", ft_strjoin("teste", "teste"));
-        return (0);
-}
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,48 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: apuchill <apuchill@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/27 21:24:14 by apuchill          #+#    #+#             */
-/*   Updated: 2020/02/14 00:27:46 by apuchill         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
-#include <stdio.h>
-
-size_t	ft_strlen(const char *s)
-{
-	size_t l;
-
-	l = 0;
-	while (s[l] != 0)
-		l++;
-	return (l);
-}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*new_str;
+	char *sub;
 	size_t	i;
-	size_t	j;
 
-	if (!s || !(new_str = (char *)malloc(len + 1)))
+	if (!s || !(sub = (char *)malloc(len +1)))
 		return (0);
-	i = start;
-	j = 0;
-	while (i < ft_strlen(s) && j < len)
-		new_str[j++] = s[i++];
-	new_str[j] = '\0';
-	return (new_str);
-}
-
-int main ()
-{
-        char *teste;
-
-	*teste = *ft_substr("testeteste", 4, 4);
-	printf("%s", teste);
+	i = 0;
+	while (len > 0 && s[start] != '\0')
+	{
+		sub[i++] = s[start++];
+		len--;
+	}
+	sub[i] = '\0';
+	return (sub);	
 }

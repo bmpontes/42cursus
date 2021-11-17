51c69e90116b80c49c366f0e8b4ce81469bfdf46
bmpontes <bernardopontes97@gmail.com>
2021-11-17 16:33:42 +0000

    Add ft_split

--- ft_split.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-char **ft_split(char const *s, char s)
-{
-
-
-}
--- libft/main.c
+++ libft/main.c
@@ -3,6 +3,6 @@
 
 int	main(void)
 {
-	printf("%s\n", ft_strtrim("asdfghjklç", "asdfç"));
+	printf("%s\n", *ft_split("daasfghjklçd", 'd'));
 	return (0);
 }
